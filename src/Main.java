27791c663e47c1b0526fe9f59bb57c868ffa5ac5
ga-ni <ggaeun3075@gmail.com>
2019-04-15 23:41:34 +0900

    문자열까지

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
+        char a1 = 'a';
+        char a2 = 97;
+        char a3 = '\u0061';
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
 
-        System.out.println("Hello World!");
+
+        String str1 = "hello";
+        String str2 = "java";
+        String str3 = "hello";
+
+        if (str1.equals(str2)){
+            System.out.println(str1 + "&" + str2);
+        }else if(str1.equals(str3)){
+            System.out.println(str1 + "&" + str3);
+        }
+
     }
 }
